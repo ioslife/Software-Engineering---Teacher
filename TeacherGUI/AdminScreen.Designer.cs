a0ce0d64f0490b6f7bdb3b3f05c069dcb6c8aef5
Bronson Lane <bronsonglane@me.com>
2018-11-14 14:23:02 -0500

    populate professor dropdown on admin screen

--- TeacherGUI/AdminScreen.Designer.cs
+++ TeacherGUI/AdminScreen.Designer.cs
@@ -129,6 +129,7 @@
             this.comboBox1.Size = new System.Drawing.Size(260, 21);
             this.comboBox1.TabIndex = 10;
             this.comboBox1.Text = "Professor";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // dataGridView1
             // 
--- TeacherGUI/AdminScreen.cs
+++ TeacherGUI/AdminScreen.cs
@@ -22,21 +22,47 @@ namespace TeacherGUI
             
         }
 
-        //on load connect to DB and populate admin table
+        //on load
         private void AdminScreen_Load(object sender, EventArgs e)
         {
+
+            //populate dataGridView
             databaseController.dbConnect();
             databaseController.sqlQuery = "SELECT CONCAT(t.first_name, ' ', t.last_name) AS 'Professor', " +
-                                                  "c.name 'Class Name', co.course_day 'Class Day', c.startTime 'Class Time' " +
+                                                  "c.name 'Class Name', co.course_day 'Class Day', c.startTime 'Start Time' " +
                                                   "FROM course c " +
                                                   "JOIN course_occurrence co ON c.id = co.course_id " +
                                                   "JOIN teacher t ON c.teacher_id = t.id;";
             MySqlCommand cmd = new MySqlCommand(databaseController.sqlQuery, databaseController.conn);
             MySqlDataAdapter myAdapter = new MySqlDataAdapter();
             myAdapter.SelectCommand = cmd;
-            DataTable dTable = new DataTable();
-            myAdapter.Fill(dTable);
-            dataGridView1.DataSource = dTable;
+            DataTable dgvDataTable = new DataTable();
+            myAdapter.Fill(dgvDataTable);
+            dataGridView1.DataSource = dgvDataTable;
+
+
+
+            //populate professor dropdown
+            databaseController.sqlQuery = "SELECT CONCAT(first_name, ' ', last_name) AS 'Professor'" +
+                                          "FROM teacher";
+            cmd = new MySqlCommand(databaseController.sqlQuery, databaseController.conn);
+            MySqlDataReader reader;
+
+            reader = cmd.ExecuteReader();
+            DataTable profDataTable = new DataTable();
+            profDataTable.Columns.Add("teacher_ID", typeof(int));
+            profDataTable.Columns.Add("Professor", typeof(string));
+            profDataTable.Load(reader);
+
+            comboBox1.ValueMember = "teacher_ID";
+            comboBox1.DisplayMember = "Professor";
+            comboBox1.DataSource = profDataTable;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
