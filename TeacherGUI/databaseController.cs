8dcb951d7f88aa0e6cd72f675db93ce99f8a4ff6
Bronson Lane <bronsonglane@me.com>
2018-11-14 16:51:49 -0500

    removes unused dependencies

--- TeacherGUI/databaseController.cs
+++ TeacherGUI/databaseController.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data;
 using MySql.Data.MySqlClient;
 
 namespace TeacherGUI
